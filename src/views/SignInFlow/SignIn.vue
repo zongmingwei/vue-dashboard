7b240d62562afc466e32976c78216aa1a2537b74
zongmingwei <weizong.ming@163.com>
2019-09-23 04:30:33 +0800

    first commit

---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="container"
+    :class="{'light-background' : !isDarkMode, 'dark-background' : isDarkMode}"
+  >
+    <RequestAccount/>
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" v-show="isDarkMode">
+      <img src="@/assets/DCHQ-dark.svg" v-show="!isDarkMode">
+      <h4 :class="{'light-text' : isDarkMode, 'dark-text' : !isDarkMode}">Sign into Design+Code HQ</h4>
+      <input
+        type="email"
+        placeholder="Email"
+        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+      >
+      <input
+        type="password"
+        placeholder="Password"
+        :class="{'light-field' : isDarkMode, 'dark-field' : !isDarkMode}"
+      >
+      <button>Sign In</button>
+      <router-link
+        to="/recover"
+        :class="{'light-link': isDarkMode, 'dark-link' : !isDarkMode}"
+      >Forgot your password?</router-link>
+      <ThemeSwitch/>
+    </div>
+  </div>
+</template>
+
+<script>
+import RequestAccount from "@/components/RequestAccount";
+import ThemeSwitch from "@/components/ThemeSwitch";
+
+export default {
+  name: "SignIn",
+  components: {
+    RequestAccount,
+    ThemeSwitch
+  },
+  computed: {
+    isDarkMode() {
+      return this.$store.getters.isDarkMode;
+    }
+  },
+  methods: {
+    toggleDarkMode() {
+      this.$store.commit("toggleDarkMode");
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+}
+
+.login {
+  width: 400px;
+  text-align: center;
+}
+</style>
